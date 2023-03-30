f420994dd068ee927128715b0d1e5c76e840294c
yukkop <hectic.yukkop@gmail.com>
2023-03-30 14:08:00 +0200

    feat: make bash&docker cover on exemple duplicate script

---
+++ duplicate-document.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Get the directory of the script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
+# Build the Docker image
+docker build -t duplicate-document:latest "${SCRIPT_DIR}/duplicate_document"
+
+# Parse command line arguments
+clear_image=false
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -D|--document-id) document_id="$2"; shift ;;
+        -t|--title) title="$2"; shift ;;
+        -C|--creditation-file) cred_file="$2"; shift ;;
+        -r|--clear-image) clear_image=true ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+# Check if required parameters are set
+if [[ -z "${document_id}" ]]; then
+    echo "Error: Document ID must be provided."
+    exit 1
+fi
+
+if [[ -z "${title}" ]]; then
+    echo "Error: Title must be provided."
+    exit 1
+fi
+
+if [[ -z "${cred_file}" ]]; then
+    echo "Error: Creditation file must be provided."
+    exit 1
+fi
+
+# Run the Docker container with the provided parameters
+docker run --rm \
+    -v "$(realpath "${cred_file}"):/app/credentials.json" \
+    duplicate-document:latest \
+    python duplicate_document.py \
+    -D "${document_id}" \
+    -t "${title}" \
+    -C "/app/credentials.json"
+
+# Remove the Docker image if specified
+if $clear_image; then
+    docker rmi duplicate-document:latest
+fi
